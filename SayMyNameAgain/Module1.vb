4d79446c15d46e97974c34114df03be6283407ca
Jamison Burton <jamison@burtontalk.com>
2022-01-27 08:35:47 -0700

    First Upload

--- SayMyNameAgain/Module1.vb
+++ SayMyNameAgain/Module1.vb
@@ -1,7 +1,32 @@
-﻿Module Module1
+﻿'Jamison Burton
+'RCET0265
+'Spring 2022
+'Say My Name...Again
+'https://github.com/Reimonoe17/SayMyNameAgain
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Module Module1
 
     Sub Main()
+        Dim name As String
 
+        Console.WriteLine("Please enter your name: ")
+        name = Console.ReadLine()
+
+        If name = "Emily" Then
+            Console.WriteLine("Hello Emily, how are you today?")
+        ElseIf name = "Joe" Then
+            Console.WriteLine("Let's go Brandon!")
+        ElseIf name = "Jamison" Then
+            Console.WriteLine("Hello, you handsome devil you!")
+        Else
+            Console.WriteLine("Hello " & name & ", do you like this program so far?")
+        End If
+
+        Console.ReadLine()
     End Sub
 
 End Module
